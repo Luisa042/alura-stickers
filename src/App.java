5678c877d75ecc68cbc2097212ab101fe1e9ff05
Luisa <luisa.santos042@gmail.com>
2023-04-01 13:59:21 -0300

    change Sticker Generator

--- src/App.java
+++ src/App.java
@@ -1,5 +1,4 @@
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.List;
@@ -15,7 +14,7 @@ public class App {
 
         // parse data
         JsonParser parser = new JsonParser();
-        List<Map<String, String>> movieList = parser.parse(json);
+        List<Map<String, String>> contentList = parser.parse(json);
 
         StickerGenerator generator = new StickerGenerator();
 
@@ -24,39 +23,19 @@ public class App {
         directory.mkdir();
 
         // show and manipulate data
-        for (Map<String, String> movie : movieList) {
-            String title = movie.get("title");
-            String imageURL = movie.get("image");
-            String imDbRating = movie.get("imDbRating");
+        for (Map<String, String> content : contentList) {
+            String title = content.get("title");
+            String imageURL = content.get("image");
+            String subtitle = "TESTING";
 
             // print info on terminal
             System.out.println("\u001b[1mTitle:\u001b[0m \u001b[44m " + title + " \u001b[m");
-            System.out.print("\u001b[1mRating:\u001b[0m " + imDbRating + " ");
-            
-            // print stars according to rating
-            double classification = Double.parseDouble(imDbRating);
-            int stars = (int) classification;
-            for (int n = 1; n <= stars; n++) {
-                System.out.print("⭐");
-            }
-            System.out.println("\n");
-
-            // generate subtitle and add image according to rating
-            String subtitle;
-            InputStream superposingImage;
-            if (classification >= 8) {
-                subtitle = "EXCELLENT";
-                superposingImage = new FileInputStream(new File("superposition/yippie.png"));
-            } else {
-                subtitle = "MID";
-                superposingImage = new FileInputStream(new File("superposition/empty.jpg"));
-            }
+            System.out.println();
 
             // generates stickers from url
             InputStream inputStream = new URL(imageURL).openStream();
             String stickerName = directory + "/" + title + ".png";
-            generator.create(inputStream, stickerName, subtitle, superposingImage);
+            generator.create(inputStream, stickerName, subtitle);
         }
     }      
 }
-
--- src/StickerGenerator.java
+++ src/StickerGenerator.java
@@ -2,7 +2,6 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Graphics2D;
-import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.InputStream;
@@ -11,56 +10,32 @@ import javax.imageio.ImageIO;
 
 public class StickerGenerator {
 
-    public void create(InputStream inputStream, String stickerName, String subtitle, InputStream inputSuperposingImage) throws Exception {
+    public void create(InputStream inputStream, String stickerName, String subtitle) throws Exception {
         // read image
-        // InputStream inputStream = new FileInputStream(new File("input/apple-cat.jpg"));
-        // InputStream inputStream = new URL("https://static.wikia.nocookie.net/evade-nextbot/images/0/0d/Tbh.png").openStream();
         BufferedImage sourceImage = ImageIO.read(inputStream);
 
         // create new image with transparent background and new size
         int width = sourceImage.getWidth();
         int height = sourceImage.getHeight();
-        int extension = height / 4;
-        int newHeight = height + extension;
+        int newHeight = height + height / 4;
         BufferedImage newImage = new BufferedImage(width, newHeight, BufferedImage.TRANSLUCENT);
 
-        // copy source image to new image (in memory)
+        // copy source image to new image
         Graphics2D graphics = (Graphics2D) newImage.getGraphics();
         graphics.drawImage(sourceImage, 0, 0, null);
-
-        // add superposing image
-        BufferedImage superposingImage = ImageIO.read(inputSuperposingImage);
-        int superposingImagePositionX = superposingImage.getWidth();
-        int superposingImagePositionY = newHeight - (superposingImage.getHeight() + extension / 2);
-        graphics.drawImage(superposingImage, superposingImagePositionX, superposingImagePositionY, null);
        
         // font config
         Font font = new Font(Font.MONOSPACED, Font.BOLD, 72);
-        graphics.setColor(Color.white);
+        graphics.setColor(Color.WHITE);
         graphics.setFont(font);
         
         // pick string measurements
         FontMetrics metrics = graphics.getFontMetrics(font);
         int stringWidth = metrics.stringWidth(subtitle);
-        Rectangle2D textArea = metrics.getStringBounds(subtitle, graphics);
-        int stringHeight = (int) textArea.getHeight();
 
         // centralizes text
         int textPositionX = (width - stringWidth) / 2;
-        int textPositionY = newHeight - stringHeight*2;
-        
-        // font outline (it's buggy)
-        // FontRenderContext fontRenderContext = graphics.getFontRenderContext();
-        // TextLayout textLayout = new TextLayout(subtitle, font, fontRenderContext);
-        // Shape outline = textLayout.getOutline(null);
-        // AffineTransform transform = graphics.getTransform();
-        // transform.translate(textPositionX, textPositionY);
-        // graphics.setTransform(transform);
-        // BasicStroke outlineStroke = new BasicStroke(width * 0.004f);
-        // graphics.setStroke(outlineStroke);
-        // graphics.setColor(Color.BLACK);
-        // graphics.draw(outline);
-        // graphics.setClip(outline);
+        int textPositionY = height + height / 10;
         
         // write text into new image
         graphics.drawString(subtitle, textPositionX, textPositionY);
